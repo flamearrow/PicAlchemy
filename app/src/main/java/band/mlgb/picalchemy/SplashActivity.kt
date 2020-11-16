c3238b7f2cbe67a14818440aaf0302a5f73530d6
Chen Cen <arrow.cen@gmail.com>
2020-11-16 01:33:39 -0800

    splash screen

---
+++ app/src/main/java/band/mlgb/picalchemy/SplashActivity.kt
@@ -0,0 +1,13 @@
+package band.mlgb.picalchemy
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Thread.sleep(350)
+        startActivity(Intent(this, AlchemyActivity::class.java))
+    }
+}
