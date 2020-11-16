383276fc3cb3d3cd0e00fd650c27ba035eee9342
Chen Cen <arrow.cen@gmail.com>
2020-11-16 00:44:55 -0800

    permission request

--- app/src/main/java/band/mlgb/picalchemy/AlchemyActivity.kt
+++ app/src/main/java/band/mlgb/picalchemy/AlchemyActivity.kt
@@ -1,7 +1,13 @@
 package band.mlgb.picalchemy
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import band.mlgb.picalchemy.databinding.ActivityEntryBinding
 import band.mlgb.picalchemy.inject.AlchemyComponent
@@ -14,12 +20,84 @@ class AlchemyActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         alchemoyComponent =
             (application as PicAlchemyApp).appComponent.alchemyComponentFactory().create()
-        // no need to inject here
-        // alchemoyComponent.inject(this)
+        checkAndRequestForPermission()
+    }
 
+
+    private fun requestRequiredPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun checkAndRequestForPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_DENIED
+        ) {
+            // This returns true if user ever chosen 'deny and never ask again'
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                )
+            ) {
+                showPermissionRationale()
+            } else {
+                requestRequiredPermissions()
+            }
+        } else {
+            // Different ways to data binding
+            // setContentView(ActivityEntryBinding.inflate(layoutInflater).root)
+            DataBindingUtil.setContentView<ActivityEntryBinding>(this, R.layout.activity_entry)
+        }
+    }
+
+    private fun showPermissionRationale() {
+        AlertDialog.Builder(this).let { alertDialogBuilder ->
+            alertDialogBuilder.setMessage(getString(R.string.permission_rationale))
+            alertDialogBuilder.setPositiveButton(
+                getString(R.string.okay)
+            ) { _, _ ->
+                requestRequiredPermissions()
+            }
+            alertDialogBuilder.setOnCancelListener {
+                requestRequiredPermissions()
+            }
+
+            alertDialogBuilder.create().also {
+                it.show()
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isEmpty() ||
+                grantResults[0] != PackageManager.PERMISSION_GRANTED
+            ) {
+                Toast.makeText(
+                    this,
+                    getString(R.string.continue_without_external_read),
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
         // Different ways to data binding
         // setContentView(ActivityEntryBinding.inflate(layoutInflater).root)
         DataBindingUtil.setContentView<ActivityEntryBinding>(this, R.layout.activity_entry)
+
     }
 
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 1 shl 1
+    }
 }
